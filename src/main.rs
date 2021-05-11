6b39e5b50076a7e1ee3ad8e75d25da82160aca35
ggoraa <ggoraa1029@gmail.com>
2021-05-11 11:34:39 +0300

    add basic code in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod prepare_build;
+
+use switch_statement;
+
+const BUILD_COMMAND: &str = "build";
+
 fn main() {
-    println!("Hello, world!");
+    println!("GarBuild");
+
+    let command = std::env::args().nth(1).expect("command");
+
+    // Check what command was in here
+    switch_statement::switch! { command;
+        BUILD_COMMAND => {
+            println!("Building project...");
+        },
+        _ => println!("No command found."),
+    }
 }
