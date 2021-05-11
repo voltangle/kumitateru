0b33bb4a001bda8024425d120c77179ca3d2169e
ggoraa <ggoraa1029@gmail.com>
2021-05-11 11:34:17 +0300

    add prepare_build.rs for preparing the build

---
+++ src/prepare_build.rs
@@ -0,0 +1,7 @@
+
+/// This function gathers all files from resources and
+/// src directories, and transfers them in build/proj,
+/// where it will be built by monkeyc.
+fn construct_connectiq_project() {
+
+}
