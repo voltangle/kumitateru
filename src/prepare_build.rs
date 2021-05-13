9ade2c89b9257a348e15b0ee4c332eae39c9f331
ggoraa <ggoraa1029@gmail.com>
2021-05-13 21:08:37 +0300

    updated project generator

--- src/prepare_build.rs
+++ src/prepare_build.rs
@@ -1,9 +1,18 @@
-use crate::utils::verify_project::verify_project;
+use colored::Colorize;
+use std::fs;
+use std::path::Path;
 
 /// This function gathers all files from resources and
 /// src directories, and transfers them in build/proj,
 /// where it will be built by monkeyc.
-pub fn construct_connectiq_project() {
-    // Step 1: Verify project structure
-    verify_project();
+pub fn construct_connectiq_project() -> Result<bool, std::io::Error> {
+    fs::create_dir("build");
+    fs::create_dir("build/tmp");
+    fs::create_dir("build/tmp/source");
+    fs::create_dir("build/tmp/resources");
+
+    println!("{}", "Transferring basic files and source code...".bold());
+
+    fs::copy("manifest.xml", "build/tmp/manifest.xml")?;
+    Ok(true)
 }
