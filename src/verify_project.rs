9214a4201d3cae4bd605f87f2062855d53b4e92a
ggoraa <ggoraa1029@gmail.com>
2021-05-17 11:38:29 +0300

    updated dependencies in verify_project.rs

--- src/verify_project.rs
+++ src/verify_project.rs
@@ -3,7 +3,7 @@ use std::fs;
 use colored::Colorize;
 use std::path::PathBuf;
 use crate::utils::do_vectors_match::do_vectors_match;
-use minidom::Element;
+use crate::utils::manifest_utils::{get_devices_from_manifest, get_languages_from_manifest};
 
 pub fn verify_project() {
     let mut manifest_location: PathBuf;
@@ -128,63 +128,4 @@ fn match_device_resources(manifest: Vec<String>, res: Vec<String>) {
             }
         }
     }
-}
-
-fn get_languages_from_manifest(manifest: String) -> Vec<String> {
-    println!("{}", "Checking languages...".bold());
-    let root: Element = manifest.parse().unwrap();
-    let mut languages: Vec<String> = Vec::new();
-
-    for children in root.children() {
-        if children.is("application", "http://www.garmin.com/xml/connectiq") {
-            let language_children = children.get_child("languages", "http://www.garmin.com/xml/connectiq");
-            match language_children {
-                None => {
-                    eprintln!("{} {} {} {}?", "No languages found in manifest.xml.".red(), "Have you added an".bold(), "<iq:languages>".bold().green(), "block".bold());
-                    std::process::exit(1);
-                }
-                Some(element) => {
-                    for child in element.children() {
-                        println!("{} {}", "Detected a language:", child.text().bold().green());
-                        languages.push(child.text());
-                    }
-                    if languages.is_empty() {
-                        eprintln!("{} {} {} {}?", "No languages found in manifest.xml.".red(), "Have you".bold(), "declared".bold().green(), "any".bold());
-                        std::process::exit(1);
-                    }
-                }
-            }
-        }
-    }
-
-    return languages;
-}
-
-fn get_devices_from_manifest(manifest: String) -> Vec<String> {
-    println!("{}", "Checking devices...".bold());
-    let root: Element = manifest.parse().unwrap();
-    let mut devices: Vec<String> = Vec::new();
-
-    for children in root.children() {
-        if children.is("application", "http://www.garmin.com/xml/connectiq") {
-            let products_children = children.get_child("products", "http://www.garmin.com/xml/connectiq");
-            match products_children {
-                None => {
-                    eprintln!("{} {} {} {}?", "No products found in manifest.xml.".red(), "Have you added an".bold(), "<iq:products>".bold().green(), "block".bold());
-                    std::process::exit(1);
-                }
-                Some(element) => {
-                    for child in element.children() {
-                        devices.push(child.text());
-                    }
-                    if devices.is_empty() {
-                        eprintln!("{} {} {} {}?", "No products found in manifest.xml.".red(), "Have you".bold(), "declared".bold().green(), "any".bold());
-                        std::process::exit(1);
-                    }
-                }
-            }
-        }
-    }
-
-    return devices;
 }
