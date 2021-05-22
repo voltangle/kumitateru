1c66e2f4db1da0aaa45ec0ec3b326d8ad87e8139
ggoraa <ggoraa1029@gmail.com>
2021-05-22 17:11:43 +0300

    Moved to clap for processing command line args

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,45 @@ mod prepare_build;
 mod utils;
 mod verify_project;
 
-use switch_statement;
 use colored::Colorize;
 use std::fs;
 use crate::verify_project::verify_project;
 use crate::prepare_build::construct_connectiq_project;
 use crate::utils::manifest_utils::generate_ciq_manifest;
+use clap::{Arg, SubCommand, App};
 
-const BUILD_COMMAND: &str = "build";
 
 fn main() {
-    let command: String;
-    match std::env::args().nth(1) {
-        None => {
-            eprintln!("{}", "No command was passed to Kumitateru. Exiting...".bright_red().bold());
-            std::process::exit(1);
-        }
-        Some(_command) => {
-            command = _command;
-        }
-    }
+    let matches = App::new("Kumitateru")
+        .version("0.1.0")
+        .author("GGorAA <yegor_yakovenko@icloud.com>")
+        .about("A build system for Garmin ConnectIQ.")
+        .arg(Arg::with_name("version")
+            .value_name("version")
+            .help("Get version")
+            .takes_value(false))
+        .subcommand(SubCommand::with_name("build"))
+        .get_matches();
 
-    // Check what command was in here
-    switch_statement::switch! { command;
-        BUILD_COMMAND => {
-            println!("Building project...");
-            println!("{} {}", "Step 1:".bold().bright_green(), "Verify project structure");
-            verify_project();
-            println!("{} {}", "Step 2:".bold().bright_green(), "Assemble a ConnectIQ Project");
-            construct_connectiq_project(
-                generate_ciq_manifest(fs::read_to_string("kumitateru.toml").unwrap())
-            );
-        },
-        _ => println!("No command found."),
+
+    match matches.subcommand_name() {
+        Some(name) => {
+            match name {
+                "build" => {
+                    println!("Building project...");
+                    println!("{} {}", "Step 1:".bold().bright_green(), "Verify project structure");
+                    verify_project();
+                    println!("{} {}", "Step 2:".bold().bright_green(), "Assemble a ConnectIQ Project");
+                    construct_connectiq_project(
+                        generate_ciq_manifest(fs::read_to_string("kumitateru.toml").unwrap())
+                    );
+                }
+                &_ => {}
+            }
+        }
+        None => {
+            println!("{}", matches.usage());
+        }
     }
 }
 
