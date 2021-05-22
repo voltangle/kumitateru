8fb697f15798055a4b52701eaf93100c154bcfb2
ggoraa <ggoraa1029@gmail.com>
2021-05-22 20:57:35 +0300

    Added an ability to compile for a specific device

--- src/compile_project.rs
+++ src/compile_project.rs
@@ -3,27 +3,46 @@ use std::process::Command;
 use crate::utils::config::parse_config;
 use std::fs;
 
-pub fn compile_project(project: PathBuf, output: PathBuf) {
+pub fn compile_project(project: PathBuf, output: PathBuf, target: &str) {
     let mut jungle_path = project;
     jungle_path.push("monkey.jungle");
     let mut output_path = output;
     let parsed_config = parse_config(fs::read_to_string("kumitateru.toml").unwrap());
     output_path.push(parsed_config.package.name);
-    println!("{:?}", ["monkeyc", "--jungles", jungle_path.to_str().unwrap(),
-        "--package-app",
-        "--release",
-        "--output", output_path.to_str().unwrap(),
-        "--private-key", &parsed_config.build.signing_key,
-        "--warn"]);
-    Command::new("monkeyc")
-        .args(&[
-            "--jungles", jungle_path.to_str().unwrap(),
-            "--package-app",
-            "--release",
-            "--output", output_path.to_str().unwrap(),
-            "--private-key", &parsed_config.build.signing_key,
-            "--warn"
-        ])
-        .output()
-        .expect("Failed to run monkeyc.");
+
+
+    if target == "package" {
+        Command::new("monkeyc")
+            .args(&[
+                "--jungles", jungle_path.to_str().unwrap(),
+                "--package-app",
+                "--release",
+                "--output", output_path.to_str().unwrap(),
+                "--private-key", &parsed_config.build.signing_key,
+                "--warn"
+            ])
+            .output()
+            .expect("Failed to run monkeyc.");
+    } else {
+        println!("{}", jungle_path.to_str().unwrap());
+        println!("{}", output_path.to_str().unwrap());
+        let output = Command::new("monkeyc")
+            .args(&[
+                "--jungles", jungle_path.to_str().unwrap(),
+                "--device", target,
+                "--output", output_path.to_str().unwrap(),
+                "--private-key", &parsed_config.build.signing_key,
+                "--warn"
+            ])
+            .output()
+            .expect("Failed to run monkeyc.");
+        println!("{}", String::from_utf8_lossy(&output.stderr))
+    }
 }
+
+// monkeyc \
+// --jungles ./monkey.jungle \
+// --device $(DEVICE) \
+// --output bin/$(appName).prg \
+// --private-key $(PRIVATE_KEY) \
+// --warn
