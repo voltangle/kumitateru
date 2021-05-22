e2c56336da434302c65e0b659a6a97e8c0da2d0d
ggoraa <ggoraa1029@gmail.com>
2021-05-22 17:14:17 +0300

    added: - Creation of manifest and .jungle files - Functionality to move main resources to "resources" dir

--- src/prepare_build.rs
+++ src/prepare_build.rs
@@ -18,6 +18,9 @@ pub fn construct_connectiq_project(manifest: String) {
     let _ = fs::File::create(PathBuf::from("build/tmp/manifest.xml"));
     let _ = fs::write(PathBuf::from("build/tmp/manifest.xml"), manifest);
 
+    let _ = fs::File::create(PathBuf::from("build/tmp/monkey.jungle"));
+    let _ = fs::write(PathBuf::from("build/tmp/monkey.jungle"), r#"project.manifest = manifest.xml"#);
+
     let _ = copy(PathBuf::from("src"), PathBuf::from("build/tmp/source"));
     println!("{}", "Preparing resources...".bold());
     let mut device_specific_res: Vec<String> = Vec::new();
@@ -37,7 +40,7 @@ pub fn construct_connectiq_project(manifest: String) {
     }
 
     // And create directories
-    for dir in device_specific_res {
+    for dir in &device_specific_res {
         let mut end_dir = PathBuf::from("build/tmp");
         let mut end_dirname: String = "resources-".parse().unwrap();
         end_dirname.push_str(&*dir);
@@ -45,12 +48,54 @@ pub fn construct_connectiq_project(manifest: String) {
         let _ = fs::create_dir(end_dir);
     }
 
-    // Then create directories for language resources
+    // Then create directories for language resources and transfer them
     for language in parse_config(fs::read_to_string("kumitateru.toml").unwrap()).package_meta.languages {
-        let mut end_dir = PathBuf::from("build/tmp");
-        let mut end_dirname: String = "resources-".parse().unwrap();
-        end_dirname.push_str(&*language);
-        end_dir.push(end_dirname);
-        let _ = fs::create_dir(end_dir);
+        if language == "eng" {
+            let mut end_dir = PathBuf::from("build/tmp");
+            let end_dirname: String = "resources".parse().unwrap();
+            end_dir.push(end_dirname);
+            end_dir.push("strings");
+            let _ = fs::create_dir(&end_dir);
+
+            let mut start_directory = PathBuf::from("resources/strings");
+            start_directory.push("main");
+
+            copy(start_directory, end_dir);
+        } else {
+            let mut end_dir = PathBuf::from("build/tmp");
+            let mut end_dirname: String = "resources-".parse().unwrap();
+            end_dirname.push_str(&*language);
+            end_dir.push(end_dirname);
+            let _ = fs::create_dir(&end_dir);
+
+            let mut start_directory = PathBuf::from("resources/strings");
+            start_directory.push(language);
+
+            copy(start_directory, end_dir);
+        }
     }
+
+    // And here we will transfer other resources
+    for resource in vec!["drawables", "layouts", "fonts", "menus", "settings"] {
+        transfer_device_resources(resource.to_string(), device_specific_res.clone());
+    }
+}
+
+fn transfer_device_resources(resource: String, device_specific_res: Vec<String>) {
+    for res_entry in device_specific_res {
+        let mut res_dir = PathBuf::new();
+        res_dir.push("resources");
+        res_dir.push(&resource);
+        res_dir.push(&res_entry);
+        if res_dir.exists() {
+            let mut end_dir = PathBuf::from("build/tmp");
+            let mut end_dirname = String::from("resources-");
+            end_dirname.push_str(&*res_entry);
+            end_dir.push(end_dirname);
+            end_dir.push(&resource);
+
+            copy(res_dir, &end_dir);
+        }
+    }
+
 }
