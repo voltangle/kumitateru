2589d5aa460968849023eb4bb513ced1fa5b68d4
ggoraa <ggoraa1029@gmail.com>
2021-07-24 17:40:03 +0300

    Replaced monkeyc usage with monkeybrains.jar

--- src/compile_project.rs
+++ src/compile_project.rs
@@ -3,64 +3,79 @@ use std::process::Command;
 use crate::ser_de::parse_config::parse_config;
 use std::fs;
 
-pub fn compile_app_project(project: PathBuf, output: PathBuf, target: &str) {
+pub fn compile_app_project(project: PathBuf, output: PathBuf, target: &str, compiler: PathBuf) {
     let mut jungle_path = project;
     jungle_path.push("monkey.jungle");
     let mut output_path = output;
     let parsed_config = parse_config(fs::read_to_string("kumitateru.toml").unwrap());
 
-    if target == "package" {
-        output_path.push(parsed_config.package_meta.name);
-        output_path.set_extension("iq");
-        let output = Command::new("monkeyc")
-            .args(&[
-                "--jungles", jungle_path.to_str().unwrap(),
-                "--package-app",
-                "--release",
-                "--output", output_path.to_str().unwrap(),
-                "--private-key", &parsed_config.build.signing_key,
-                "--warn"
-            ])
-            .output()
-            .expect("Failed to run monkeyc.");
-        println!("{}", String::from_utf8_lossy(&output.stderr))
-    } else if target == "all" {
-        for device in parsed_config.package_meta.devices {
-            let mut output_path = output_path.clone();
-            let mut app_name = String::from(parsed_config.package_meta.name.clone());
-            app_name.push_str("-");
-            app_name.push_str(&*device);
-            output_path.push(app_name);
-            output_path.set_extension("prg");
-            let _ = Command::new("monkeyc")
-                .args(&[
-                    "--jungles", jungle_path.to_str().unwrap(),
-                    "--device", &device,
-                    "--output", output_path.to_str().unwrap(),
-                    "--private-key", &parsed_config.build.signing_key,
+    match target {
+        "package" => {
+            output_path.push(parsed_config.package_meta.name);
+            output_path.set_extension("iq");
+            let compiler = compiler.to_str().unwrap();
+            let monkeybrains_path: &str = &format!("{}/{}", compiler, "monkeybrains.jar");
+            println!("{}", String::from_utf8_lossy(&Command::new("java")
+                .args([
+                    "-Xms768m",
+                    "-Dfile.encoding=UTF-8",
+                    "-Dapple.awt.UIElement=true",
+                    "-jar", monkeybrains_path,
+                    "-o",output_path.to_str().unwrap(),
+                    "--package-app",
+                    "--release",
+                    "-f", jungle_path.to_str().unwrap(),
+                    "-y", &parsed_config.build.signing_key,
+                    "-l", &parsed_config.build.type_check_level.to_string(),
                     "--warn"
-                ]).spawn().unwrap();
+                ]).output().expect("Failed to run Monkey C compiler.").stderr))
         }
-    } else {
-        output_path.push(parsed_config.package_meta.name);
-        output_path.set_extension("prg");
-        let output = Command::new("monkeyc")
-            .args(&[
-                "--jungles", jungle_path.to_str().unwrap(),
-                "--device", target,
-                "--output", output_path.to_str().unwrap(),
-                "--private-key", &parsed_config.build.signing_key,
-                "--warn"
-            ])
-            .output()
-            .expect("Failed to run monkeyc.");
-        println!("{}", String::from_utf8_lossy(&output.stderr))
+        "all" => {
+            for device in parsed_config.package_meta.devices {
+                let mut output_path = output_path.clone();
+                let mut app_name = String::from(parsed_config.package_meta.name.clone());
+                app_name.push_str("-");
+                app_name.push_str(&*device);
+                output_path.push(app_name);
+                output_path.set_extension("prg");
+                let compiler = compiler.to_str().unwrap();
+                let monkeybrains_path: &str = &format!("{}/{}", compiler, "monkeybrains.jar");
+                println!("{}", String::from_utf8_lossy(&Command::new("java")
+                    .args([
+                        "-Xms768m",
+                        "-Dfile.encoding=UTF-8",
+                        "-Dapple.awt.UIElement=true",
+                        "-jar", monkeybrains_path,
+                        "-o",output_path.to_str().unwrap(),
+                        "--device", target,
+                        "-f", jungle_path.to_str().unwrap(),
+                        "-y", &parsed_config.build.signing_key,
+                        "-l", &parsed_config.build.type_check_level.to_string(),
+                        "--warn"
+                    ]).output().expect("Failed to run Monkey C compiler.").stderr))
+            }
+        }
+        _ => {
+            output_path.push(parsed_config.package_meta.name);
+            output_path.set_extension("prg");
+            let compiler = compiler.to_str().unwrap();
+            let monkeybrains_path: &str = &format!("{}/{}", compiler, "monkeybrains.jar");
+            println!("{}", String::from_utf8_lossy(&Command::new("java")
+                .args([
+                    "-Xms768m",
+                    "-Dfile.encoding=UTF-8",
+                    "-Dapple.awt.UIElement=true",
+                    "-jar", monkeybrains_path,
+                    "-o",output_path.to_str().unwrap(),
+                    "--device", target,
+                    "-f", jungle_path.to_str().unwrap(),
+                    "-y", &parsed_config.build.signing_key,
+                    "-l", &parsed_config.build.type_check_level.to_string(),
+                    "--warn"
+                ]).output().expect("Failed to run Monkey C compiler.").stderr))
+        }
     }
 }
 
-// monkeyc \
-// --jungles ./monkey.jungle \
-// --device $(DEVICE) \
-// --output bin/$(appName).prg \
-// --private-key $(PRIVATE_KEY) \
-// --warn
+// java -jar monkeybrains.jar -o wl.prg -f /monkey.jungle -y /id_rsa_garmin.der -l 1
+
