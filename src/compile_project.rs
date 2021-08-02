00a42749500fc17a51692803d519091a0450c937
ggoraa <ggoraa1029@gmail.com>
2021-08-02 11:23:24 +0300

    added an argument for supplying parsed config

--- src/compile_project.rs
+++ src/compile_project.rs
@@ -3,19 +3,19 @@ use std::process::Command;
 use crate::ser_de::parse_config::parse_config;
 use std::{fs, process, env};
 use anyhow::{Result, Context};
+use crate::ser_de::config::app_config::AppConfig;
 
-pub fn compile_app_project(project: PathBuf, output: PathBuf, target: &str, compiler: PathBuf) -> Result<()> {
+pub fn compile_app_project(project: PathBuf, output: PathBuf, target: &str, bin_path: PathBuf, config: AppConfig) -> Result<()> {
     let mut jungle_path = project;
     jungle_path.push("monkey.jungle");
     let mut output_path = output;
-    let parsed_config = parse_config(fs::read_to_string("package.toml").with_context(|| "Unable to read package.toml")?);
 
     match target {
         "package" => {
-            output_path.push(parsed_config.package_meta.name);
+            output_path.push(config.package_meta.name);
             output_path.set_extension("iq");
-            let compiler = compiler.to_str().unwrap();
-            let monkeybrains_path: &str = &format!("{}/{}", compiler, "monkeybrains.jar");
+            let bin_path = bin_path.to_str().unwrap();
+            let monkeybrains_path: &str = &format!("{}/{}", bin_path, "monkeybrains.jar");
             let mut command = Command::new("java");
             command.args([
                     "-Xms768m",
@@ -26,25 +26,25 @@ pub fn compile_app_project(project: PathBuf, output: PathBuf, target: &str, comp
                     "--package-app",
                     "--release",
                     "-f", jungle_path.to_str().unwrap(),
-                    "-y", &parsed_config.build.signing_key,
-                    "-l", &parsed_config.build.type_check_level.to_string(),
+                    "-y", &config.build.signing_key,
+                    "-l", &config.build.type_check_level.to_string(),
                     "--warn"
                 ]);
-            println!("{}", String::from_utf8_lossy(&*command.output().expect("Failed to run Monkey C compiler.").stderr));
-            if !command.status().unwrap().success() {
+            println!("{}", String::from_utf8_lossy(&*command.output().with_context(|| "Failed to run Monkey C compiler")?.stderr));
+            if !command.status().with_context(|| "Unable to get compiler process exit code")?.success() {
                 if !env::var("KMTR_IDE_SILENT").is_ok() { eprintln!("Build failed."); }
                 process::exit(30); // code 30 signifies that the compiler was not able to build the project
             }
         }
         "all" => {
-            for device in parsed_config.package_meta.devices {
+            for device in config.package_meta.devices {
                 let mut output_path = output_path.clone();
-                let mut app_name = String::from(parsed_config.package_meta.name.clone());
+                let mut app_name = String::from(config.package_meta.name.clone());
                 app_name.push_str("-");
                 app_name.push_str(&*device);
                 output_path.push(app_name);
                 output_path.set_extension("prg");
-                let compiler = compiler.to_str().unwrap();
+                let compiler = bin_path.to_str().unwrap();
                 let monkeybrains_path: &str = &format!("{}/{}", compiler, "monkeybrains.jar");
                 println!("{}", String::from_utf8_lossy(&Command::new("java")
                     .args([
@@ -55,16 +55,16 @@ pub fn compile_app_project(project: PathBuf, output: PathBuf, target: &str, comp
                         "-o",output_path.to_str().unwrap(),
                         "--device", target,
                         "-f", jungle_path.to_str().unwrap(),
-                        "-y", &parsed_config.build.signing_key,
-                        "-l", &parsed_config.build.type_check_level.to_string(),
+                        "-y", &config.build.signing_key,
+                        "-l", &config.build.type_check_level.to_string(),
                         "--warn"
                     ]).output().expect("Failed to run Monkey C compiler.").stderr))
             }
         }
         _ => {
-            output_path.push(parsed_config.package_meta.name);
+            output_path.push(config.package_meta.name);
             output_path.set_extension("prg");
-            let compiler = compiler.to_str().unwrap();
+            let compiler = bin_path.to_str().unwrap();
             let monkeybrains_path: &str = &format!("{}/{}", compiler, "monkeybrains.jar");
             let mut command = Command::new("java").args([
                 "-Xms768m",
@@ -74,8 +74,8 @@ pub fn compile_app_project(project: PathBuf, output: PathBuf, target: &str, comp
                 "-o",output_path.to_str().unwrap(),
                 "--device", target,
                 "-f", jungle_path.to_str().unwrap(),
-                "-y", &parsed_config.build.signing_key,
-                "-l", &parsed_config.build.type_check_level.to_string(),
+                "-y", &config.build.signing_key,
+                "-l", &config.build.type_check_level.to_string(),
                 "--warn"
             ]).spawn().expect("Failed to run Monkey C compiler.");
             if !command.wait().unwrap().success() {
